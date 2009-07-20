694a127cc2e022161a4833d4a165f0bdd6d1012b
Christoph Bünte <info@christophbuente.de>
2009-07-20 14:14:39 +0200

    added some more function wrappers

--- lib/xtract.rb
+++ lib/xtract.rb
@@ -6,7 +6,27 @@ module Xtract
   
   include Vector
   include Scalar
+  
+  XTRACT_BARK_BANDS = 26
+  XTRACT_NONE = 0
+  XTRACT_ANY = -1
+  XTRACT_UNKNOWN = -2
+  XTRACT_MAXARGS = 4
+  XTRACT_MAX_NAME_LENGTH = 64
+  XTRACT_MAX_AUTHOR_LENGTH = 128
+  XTRACT_MAX_DESC_LENGTH = 256
+  
+  class AmpTracker < FFI::Struct
+    layout  :count,         :int,
+            :previous_amp,  :float
+  end
+  
+  class FrameTracker < FFI::Struct
+    layout  :frame_count,     :int,
+            :previous_frame,  :float
+  end
+  
+  attach_function 'xtract_init_mfcc', [:int, :float, :int, :float, :float, :int, :pointer), :int
+  
+  
 end
-
-# require 'rubygems'
-# require 'xtract'
--- lib/xtract/vector.rb
+++ lib/xtract/vector.rb
@@ -3,6 +3,13 @@ module Xtract
       extend FFI::Library
       ffi_lib 'libxtract'
 
+      # Extract normalized (0-1) frequency domain spectrum from time domain signal.
+      attach_function 'xtract_spectrum',[:pointer, :int, :pointer, :pointer), :int
+
+      # Extract Average Magnitude Difference Function from time domain signal. 
       attach_function 'xtract_amdf',[:pointer, :int, :pointer, :float],  :int
+      
+      # Extract Mel Frequency Cepstral Coefficients based on a method described by Rabiner.
+      attach_function 'xtract_mfcc', [:pointer, :int, :pointer, :pointer], int
   end
 end
